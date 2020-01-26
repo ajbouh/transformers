666a3dceacf35e3887a0d852cae264d5ea2ad32c
Adam Bouhenguel <adam@bouhenguel.com>
2020-01-25 16:58:30 -0800

    Try this

--- transformers/modeling_albert.py
+++ transformers/modeling_albert.py
@@ -241,11 +241,11 @@ class AlbertAttention(BertSelfAttention):
 
         # Should find a better way to do this
         w = self.dense.weight.t().reshape(self.num_attention_heads, self.attention_head_size, self.hidden_size).to(context_layer.dtype)
-        w = w.cpu()
+        w = w.to_dense()
         b = self.dense.bias.to(context_layer.dtype)
 
-        print("context_layer", context_layer)
-        print("w", w)
+        print("context_layer", context_layer.layout, context_layer.device)
+        print("w", w.layout, w.device)
         projected_context_layer = torch.einsum("bfnd,ndh->bfh", context_layer, w) + b
         projected_context_layer_dropout = self.dropout(projected_context_layer)
         layernormed_context_layer = self.LayerNorm(input_ids + projected_context_layer_dropout)
